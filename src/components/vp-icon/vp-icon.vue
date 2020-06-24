5435b873b1050014aa6290a32b0a714831c30105
wupup <1250909725@qq.com>
2020-06-24 12:52:06 +0800

    init

---
+++ src/components/vp-icon/vp-icon.vue
@@ -0,0 +1,27 @@
+<!--
+ * @Autor: wupup
+ * @Description: 
+ * @Date: 2020-06-24 12:25:08
+ * @LastEditors: wupup
+ * @LastEditTime: 2020-06-24 12:45:42
+ * @FilePath: \vpart\src\components\vp-icon\vp-icon.vue
+-->
+
+<template>
+  <div class="icon">
+    <p>icon component</p>
+    <i>icon</i>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<!--
+ * @Autor: wupup
+ * @Description: 
+ * @Date: 2020-06-24 12:07:00
+ * @LastEditors: wupup
+ * @LastEditTime: 2020-06-24 12:48:55
+ * @FilePath: \vpart\src\views\Home.vue
+-->
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <vp-icon />
+  </div>
+</template>
+
+<script>
+import VpIcon from "@/components/vp-icon/vp-icon.vue";
+
+export default {
+  name: "Home",
+  components: {
+    VpIcon
+  }
+};
+</script>
