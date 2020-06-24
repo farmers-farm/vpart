dd25b11b2397faeb9ae511eb2053f66671403ad6
wupup <1250909725@qq.com>
2020-06-24 13:03:55 +0800

    update logo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,12 +3,12 @@
  * @Description: 
  * @Date: 2020-06-24 12:07:00
  * @LastEditors: wupup
- * @LastEditTime: 2020-06-24 12:48:55
+ * @LastEditTime: 2020-06-24 13:02:48
  * @FilePath: \vpart\src\views\Home.vue
 -->
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <img alt="Vue logo" src="../assets/logo.jpg" />
     <vp-icon />
   </div>
 </template>
